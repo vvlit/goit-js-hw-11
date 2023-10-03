4c466185bc2fe84a5c8a3dbda12036f73c8785f7
vvlit <43747693+vvlit@users.noreply.github.com>
2023-10-03 10:58:48 +0000

    Deploying to gh-pages from @ vvlit/goit-js-hw-11@a54a3c8c321bd23ce0328a023893eb052385540d 🚀

---
+++ index.93fed3d9.js
@@ -0,0 +1,2 @@
+document.querySelector(".search-form");
+//# sourceMappingURL=index.93fed3d9.js.map
---
+++ index.ae71ad6c.js
@@ -0,0 +1,2 @@
+document.querySelector(".search-form");
+//# sourceMappingURL=index.ae71ad6c.js.map
